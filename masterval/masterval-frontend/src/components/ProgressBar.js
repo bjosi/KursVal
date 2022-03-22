57e19fc933c4cdd2f88603bb43547cd61537d0e9
mathildawelander <78489606+mathildawelander@users.noreply.github.com>
2022-03-22 13:21:49 +0100

    gjort lite css på overview

---
+++ masterval/masterval-frontend/src/components/ProgressBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const ProgressBar = ({ progress }) => {
+    var prog = progress;
+    if (prog > 1) {
+        prog = 1;
+    }
+    prog = prog * 100;
+
+    const Childdiv = {
+        height: '100%',
+        width: `${prog}%`,
+        backgroundColor: '#F9A26C',
+        borderRadius: '0.1rem',
+        textAlign: 'right'
+    }
+
+    const progresstext = {
+        padding: 5,
+        color: 'black',
+        fontWeight: 400
+    }
+
+    return (
+        <div className="progressbar_parent" >
+            <div style={Childdiv}>
+                <span style={progresstext}></span>
+            </div>
+        </div>
+    )
+}
+
+export default ProgressBar;
