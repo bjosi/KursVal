31b5880a09495b82f23aa28e286319af75d36948
Axel <axel.nie17@gmail.com>
2022-04-13 09:38:21 +0200

    css

--- masterval/masterval-frontend/src/Pages/LogIn.js
+++ masterval/masterval-frontend/src/Pages/LogIn.js
@@ -1,83 +1,104 @@
 import { signup, useAuth, logout, login } from "../firebase";
 import "../styles/login.css";
 import { useRef, useState } from "react";
-import ToggleLoginButton from '../components/ToggleLoginButton';
-
+import ToggleLoginButton from "../components/ToggleLoginButton";
 
 export default function LogIn() {
-    const [loading, setLoading] = useState(false);
-    const currentUser = useAuth();
-    const [showlogin, setshowlogin] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const currentUser = useAuth();
+  const [showlogin, setshowlogin] = useState(false);
 
-    const emailRef = useRef();
-    const passwordRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-    async function handleSignup() {
-
-        setLoading(true);
-        try {
-            await signup(emailRef.current.value, passwordRef.current.value);
-        }
-        catch {
-            alert("Error");
-        }
-
-        setLoading(false);
+  async function handleSignup() {
+    setLoading(true);
+    try {
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      alert("Error");
     }
 
-    async function handleLogout() {
-        setLoading(true);
-        try {
-            await logout();
-        }
-        catch { alert("Error!") }
-        setLoading(false);
+    setLoading(false);
+  }
+
+  async function handleLogout() {
+    setLoading(true);
+    try {
+      await logout();
+    } catch {
+      alert("Error!");
+    }
+    setLoading(false);
+  }
+
+  async function handleLogin() {
+    setLoading(true);
+    try {
+      await login(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      alert("Error");
     }
 
+    setLoading(false);
+  }
 
-    async function handleLogin() {
+  return (
+    <>
+      <div className="testing">
+        <ToggleLoginButton
+          showOverview={showlogin}
+          setShowOverview={setshowlogin}
+        />
+      </div>
+      <div> currently logged in as: {currentUser?.email}</div>
 
-        setLoading(true);
-        try {
-            await login(emailRef.current.value, passwordRef.current.value);
-        }
-        catch {
-            alert("Error");
-        }
+      <div className="fields">
+        <input className="email_input" ref={emailRef} placeholder="Email" />
 
-        setLoading(false);
-    }
+        <input
+          className="password_input"
+          ref={passwordRef}
+          type="password"
+          placeholder="Lï¿½senord"
+        />
+      </div>
+      <div className="testingmore">
+        <div className="buttons">
+          {showlogin ? (
+            <button
+              className="Btn_LogIn"
+              disabled={loading || currentUser}
+              onClick={handleSignup}
+            >
+              {" "}
+              Registera dig{" "}
+            </button>
+          ) : (
+            <button
+              className="Btn_LogIn"
+              disabled={loading || currentUser}
+              onClick={handleLogin}
+            >
+              {" "}
+              Logga in{" "}
+            </button>
+          )}
 
-
-    return (
-        <>
-            <div className="testing">
-          
-
-            <ToggleLoginButton showOverview={showlogin} setShowOverview={setshowlogin} />
-  </div>
-            <div > currently logged in as: {currentUser?.email}
-            </div>
-
-            <div className="fields">
-
-                <input className="email_input" ref={emailRef} placeholder="Email" />
-
-                <input className="password_input" ref={passwordRef} type="password" placeholder="Lösenord" />
-            </div>
-            <div className="testingmore">
-
-            <div className="buttons">
-            {showlogin ? <button className="Btn_LogIn" disabled={loading || currentUser} onClick={handleSignup}> Registera dig </button> :
-
-                <button className="Btn_LogIn" disabled={loading || currentUser} onClick={handleLogin}> Logga in </button>
-            }
-
-            {showlogin ? <div> </div> : <button className="Btn_LogIn" disabled={loading || !currentUser} onClick={handleLogout}> Logga ut </button> }
-            </div>
-
-            </div>
-        </>
-        )
+          {showlogin ? (
+            <div> </div>
+          ) : (
+            <button
+              className="Btn_LogIn"
+              disabled={loading || !currentUser}
+              onClick={handleLogout}
+            >
+              {" "}
+              Logga ut{" "}
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
-
