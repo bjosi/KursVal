f24fb748cf2d77a960cfd67fd8b69b6b89ed2c34
Axel <axel.nie17@gmail.com>
2022-04-19 15:22:40 +0200

    css

--- masterval/masterval-frontend/src/components/DisplayCourse.js
+++ masterval/masterval-frontend/src/components/DisplayCourse.js
@@ -11,8 +11,8 @@ import {
   faAngleUp,
   faCircleHalfStroke,
   faCircle,
-  faGraduationCap,
-  faCalendar,
+  faCrosshairs,
+  faSignal,
 } from "@fortawesome/free-solid-svg-icons";
 
 const DisplayCourse = ({
@@ -44,15 +44,15 @@ const DisplayCourse = ({
             </div>
 
             <div className="c_info_container">
-              <FontAwesomeIcon className="c_info_icon" icon={faGraduationCap} />
+              <FontAwesomeIcon className="c_info_icon" icon={faCrosshairs} />
               <p className="c_info"> {courseinfo.progname} </p>
             </div>
             <div className="c_info_container">
-              <FontAwesomeIcon className="c_info_icon" icon={faCalendar} />
+              <MySvg className="c_info_icon" />
               <p className="c_info"> Termin {courseinfo.semester} </p>
             </div>
             <div className="c_info_container">
-              <FontAwesomeIcon className="c_info_icon" icon={faGraduationCap} />
+              <FontAwesomeIcon className="c_info_icon" icon={faSignal} />
               <p className="c_info"> {courseinfo.courselevel} </p>
             </div>
             {showresult ? <Results courseinfo={courseinfo} /> : null}
@@ -101,6 +101,7 @@ const Results = ({ courseinfo }) => {
   const link = "https://studieinfo.liu.se/kurs/" + courseinfo.coursecode;
   return (
     <div>
+      <p className="c_info"> {courseinfo. } HP </p>
       <p className="c_info"> {courseinfo.coursepoints} HP </p>
       <p className="c_info"> Kurskod: {courseinfo.coursecode} </p>
       <a
@@ -179,5 +180,48 @@ const ShowBlockOfCourse = ({ courseinfo }) => {
     )
 }
 */
+export const CalendarIcon = () => {
+  return (
+    <svg
+      style={{ width: "inherit", height: "inherit" }}
+      version="1.1"
+      viewBox="0 0 448 512"
+      xmlns="http://www.w3.org/2000/svg"
+      width="10%"
+    >
+      <path
+        strokeWidth="2"
+        d="M152 64H296V24C296 10.75 306.7 0 320 0C333.3 0 344 10.75 344 24V64H384C419.3 64 448 92.65 448 128V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V128C0 92.65 28.65 64 64 64H104V24C104 10.75 114.7 0 128 0C141.3 0 152 10.75 152 24V64zM48 448C48 456.8 55.16 464 64 464H384C392.8 464 400 456.8 400 448V192H48V448z"
+      />
+    </svg>
+  );
+};
+
+const MySvg = (props) => {
+  return (
+    // With Styling
+    <div
+      style={{
+        width: "20",
+        height: "20",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      className="c_info_icon"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="13"
+        height="13"
+        fill="currentColor"
+        class="bi bi-calendar"
+        viewBox="0 0 16 16"
+      >
+        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+      </svg>
+    </div>
+  );
+};
 
 export default DisplayCourse;
